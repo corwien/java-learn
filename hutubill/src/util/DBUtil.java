a664c4b29986c1208e73ef2bced0d6f3e7ffea6f
Corwien <corwien@126.com>
2020-01-11 22:58:55 +0800

    j2ee基础

--- hutubill/src/util/DBUtil.java
+++ hutubill/src/util/DBUtil.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-	private static String ip = "127.0.0.1";
-	private static int port = 3306;
-	private static String database = "hutubill";
-	private static String encoding = "UTF-8";
-	private static String loginName = "root";
-	private static String password = "518666";
+	public static String ip = "127.0.0.1";
+	public static int port = 3306;
+	public static String database = "hutubill";
+	public static String encoding = "UTF-8";
+	public static String loginName = "root";
+	public static String password = "518666";
 	
 	// 驱动初始化放在了静态初始化块里，因为这行代码需要先执行，而且只需要执行一次就足够了。
 	static {
